d506549259bb01a390065b07afb685dd3a76867a
Mahadi Hassan <itsmahadi@gmail.com>
2024-10-26 13:02:03 +0600

    update

--- web_scraper/process_sample.py
+++ web_scraper/process_sample.py
@@ -1,5 +1,3 @@
-from click import option
-from django.contrib.sitemaps.views import index
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
